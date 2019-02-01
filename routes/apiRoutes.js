bc90021109ad400e6e0f837dc53aedc36bf123cc
Parashara <parashara@gmail.com>
2019-01-31 17:16:33 -0800

    somewhat refactored code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,44 +3,44 @@ const router = require("express").Router();
 const db = require("../models")
 const apiKey = "AIzaSyDuLe6erooqM6DzaLkOEiL7d26Wq0SulS0"
 var usableObject
+
+function queryGoogleBooks(query) {
+  return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=" + apiKey, {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+}
+// This route takes in the search from user form input
+// finds 10 books from google api using that input
+// puts the books in mongoDB
+// returns the books to be rendered
 router.get("/books", (req, res) => {
-  console.log(req.query)
-  console.log(req.body)
-  console.log(("https://www.googleapis.com/books/v1/volumes?q=" + req.query.q + "&key=" + apiKey))
-  axios
-    .get("https://www.googleapis.com/books/v1/volumes?q=" + req.query.q + "&key=" + apiKey, {
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
+  const googleSearchTerm = req.query.q
+  queryGoogleBooks(googleSearchTerm)
     .then((results) => {
       var newArr = []
       for (var i = 0; i < results.data.items.length; i++) {
         newArr.push(results.data.items[i].volumeInfo)
       }
-      db.Book.remove({}).then(() => {
-        db.Book.create(newArr).then(() => {
+      db.Book.remove({})
+        .then(() => db.Book.create(newArr))
+        .then(() => {
           //trying to make an undefined image in mongoose breaks whole app, so this skips past books apis without images
           for (var i = 0; i < newArr.length; i++) {
             if (!newArr[i].imageLinks) {
               return
             }
-            db.Book.findOneAndUpdate({ 'title': newArr[i].title }, { 'image': newArr[i].imageLinks.thumbnail }, function (error, found) {
-              if (error) {
-                console.log(error);
-                res.send(error);
-              }
-              else {
+            db.Book.findOneAndUpdate({ 'title': newArr[i].title }, { 'image': newArr[i].imageLinks.thumbnail })
+              .then((found) => {
                 console.log(found)
-              }
-            })
+              })
           }
         }).then(() => {
           db.Book.find({}).then((data) => {
             res.json(data)
           })
         })
-      })
     })
     .catch(err => res.status(422).json(err));
 });
